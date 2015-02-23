9dc2795e7186a050380c3e6985439af0f981fa33
Margaux <margauxqc@yahoo.com>
2015-02-22 20:34:24 -0800

    page transition stuff

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,33 @@
 /* jshint devel:true */
 console.log('\'Allo \'Allo!');
+
+function loadUrl (url) {
+	var tempDiv = $('<div>');
+	var oldContent = $( ".content");
+	tempDiv.load( url + " .content", undefined, function () {
+		bindMagicLinks(tempDiv);
+		oldContent.after(tempDiv.find('.content'));
+		oldContent.remove();
+	});
+	history.pushState({
+		url: url
+	}, undefined, url);
+}
+
+function bindMagicLinks(target) {
+	if (target === undefined) target = $('body');
+	target.find('a.magic').click(function(event){
+		event.preventDefault();
+		loadUrl("page1.html");
+	});
+}
+
+bindMagicLinks();
+
+window.onpopstate = function (event) {
+	if (event.state && event.state.url) {
+		loadUrl(event.state.url);
+		return;
+	}
+	window.location.assign(document.location);
+}
