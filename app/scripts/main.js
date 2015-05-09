78693e2f312e11f94f2e22846f76809bfe203796
Margaux <margauxqc@yahoo.com>
2015-05-09 13:07:47 -0700

    deploy bug

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,22 +1,23 @@
 /* jshint devel:true */
 'use strict';
+if (Masonry) {
+	var container = document.querySelector('.masonry');
+	var msnry = new Masonry( container, {
 
-var container = document.querySelector('.masonry');
-var msnry = new Masonry( container, {
+		// options
+		columnWidth: 5,
+		itemSelector: '.tn',
+		gutter: 174
+	});
 
-	// options
-	columnWidth: 5,
-	itemSelector: '.tn',
-	gutter: 174
-});
+	Array.prototype.slice.call(container.querySelectorAll('.tn')).forEach(function (img) {
+		img.onload = function () {
+			msnry.layout();
+		};
+	});
 
-Array.prototype.slice.call(container.querySelectorAll('.tn')).forEach(function (img) {
-	img.onload = function () {
-		msnry.layout();
-	};
-});
-
-var img = new Image();
-img.classList.add('header-nib');
-img.src = "images/php/nib" + Math.ceil(Math.random() * 5) + ".png";
-$('.butt').before(img);
+	var img = new Image();
+	img.classList.add('header-nib');
+	img.src = "images/php/nib" + Math.ceil(Math.random() * 5) + ".png";
+	$('.butt').before(img);
+}
